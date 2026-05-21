fa620a95f2a39182a1b41caa911de9d56c08e78f
StarField Xu <air_jinkela@163.com>
2026-05-21 21:09:31 +0800

    mediatek: filogic: add Tplink 7dr7299 v1 DSA switch driver test
    Signed-off-by: StarField Xu <air_jinkela@163.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -120,6 +120,7 @@ platform_do_upgrade() {
 	tplink,tl-7dr7230-v2|\
 	tplink,tl-7dr7250-v1|\
 	tplink,tl-7dr7299-v1|\
+	tplink,tl-7dr7299-v1-dsa|\
 	tplink,tl-xdr4288|\
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -3082,6 +3082,31 @@ define Device/tplink_tl-7dr7299-v1
 endef
 TARGET_DEVICES += tplink_tl-7dr7299-v1
 
+define Device/tplink_tl-7dr7299-v1-dsa
+  DEVICE_VENDOR := TP-Link
+  DEVICE_MODEL := TL-7DR7299
+  DEVICE_VARIANT := v1 (DSA)
+  DEVICE_DTS := mt7988a-tplink-tl-7dr7299-v1-dsa
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_DTS_LOADADDR := 0x47f00000
+  DEVICE_PACKAGES := kmod-mt7992-firmware mt7988-wo-firmware \
+	kmod-rtl8261d kmod-i2c-gpio kmod-sfp kmod-usb3 automount
+  KERNEL_LOADADDR := 0x48000000
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  UBOOTENV_IN_UBI := 1
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.itb := append-kernel | \
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | append-metadata
+endef
+TARGET_DEVICES += tplink_tl-7dr7299-v1-dsa
+
 define Device/tplink_tl-xdr-common
   DEVICE_VENDOR := TP-Link
   DEVICE_DTS_DIR := ../dts
