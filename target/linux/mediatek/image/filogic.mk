953091dfd9f352a79f6e2efb3acbb4637ecadb3f
StarField Xu <air_jinkela@163.com>
2026-06-15 18:46:29 +0800

    mediatek: filogic: add support for Hiveton h87pro DSA
    Signed-off-by: StarField Xu <air_jinkela@163.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -3807,11 +3807,22 @@ define Device/hiveton_h87pro
   DEVICE_MODEL := h87pro
   DEVICE_DTS := mt7987a-hiveton-h87pro
   DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := mt7987-2p5g-phy-firmware kmod-sfp kmod-usb3 kmod-rtl8372n-dsa kmod-i2c-gpio kmod-mt7992-23-firmware \
+  DEVICE_PACKAGES := mt7987-2p5g-phy-firmware kmod-sfp kmod-usb3 kmod-i2c-gpio kmod-mt7992-23-firmware \
    swconfig blkid automount e2fsprogs f2fsck mkf2fs
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += hiveton_h87pro
+
+define Device/hiveton_h87pro-dsa
+  DEVICE_VENDOR := Hiveton
+  DEVICE_MODEL := h87pro (DSA)
+  DEVICE_DTS := mt7987a-hiveton-h87pro-dsa
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := mt7987-2p5g-phy-firmware kmod-sfp kmod-usb3 kmod-rtl8372n-dsa kmod-i2c-gpio kmod-mt7992-23-firmware \
+   blkid automount e2fsprogs f2fsck mkf2fs
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += hiveton_h87pro-dsa
 
 define Device/hiveton_h87pro-evb-nand
   DEVICE_VENDOR := Hiveton
